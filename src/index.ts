88b5cfc30f2286acd66ec0debd72948fd6fcb3d6
Serger Rutrkovsky <dr.bibelman@gmai.com>
2023-05-25 05:10:08 +0300

    setup webpack configuration

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import {someFn} from "./test";
+
+someFn(5);
+
+document.body.innerHTML = `<div>Hello worlds</div>`;
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function someFn(arg: number): string {
+    console.log('RANDOMV FUNCTION')
+    return ''
+}
